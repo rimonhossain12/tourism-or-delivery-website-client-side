b091dc7e4a6213f8e13d1049adbc000d4b76a24b
rimonspi12 <rimonmpi@gmail.com>
2022-01-16 13:45:26 +0600

    background images changes

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -13,18 +13,18 @@ const Header = () => {
     <div className="bg-img">
       <Navbar expand="lg" className="top-fixed">
         <Container>
-          <Navbar.Brand href="#home" className="text-primary fw-bold">Best travelling site</Navbar.Brand>
+          <Navbar.Brand href="#home" style={{color:'white'}} className="fw-bold">Best travelling site</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={HashLink} to="/home#home" className="text-primary fw-bold">Home</Nav.Link>
-              <Nav.Link as={HashLink} to="/showService#showService" className="text-primary fw-bold">service</Nav.Link>
-              <Nav.Link as={HashLink} to="/about#about" className="text-primary fw-bold">AboutUs</Nav.Link>
+              <Nav.Link as={HashLink} to="/home#home" style={{ color: 'white' }}  className="fw-bold">Home</Nav.Link>
+              <Nav.Link as={HashLink} style={{ color: 'white' }}  to="/showService#showService" className="fw-bold">service</Nav.Link>
+              <Nav.Link as={HashLink} to="/about#about" style={{ color: 'white' }}  className="fw-bold">AboutUs</Nav.Link>
               {
                 user.email &&
                 <>
 
-                  <Nav.Link as={HashLink} to="/order#order" className="text-primary fw-bold">MyOrders</Nav.Link>
+                  <Nav.Link as={HashLink} to="/order#order" style={{ color: 'white' }} className="fw-bold">MyOrders</Nav.Link>
                   < NavDropdown title="DashBoard" id="basic-nav-dropdown" className="text-primary fw-bold w-50">
                     <NavDropdown.Item href="#action/3.1">
                     </NavDropdown.Item>
@@ -36,7 +36,7 @@ const Header = () => {
                     </NavDropdown.Item>
                   </NavDropdown>
 
-                  < NavDropdown title="Profile" id="basic-nav-dropdown" className="text-primary fw-bold w-50">
+                  < NavDropdown style={{ color: 'white' }}  title="Profile" id="basic-nav-dropdown" className=" fw-bold w-50">
                     <NavDropdown.Item href="#action/3.1">
                       <img className="nav-img img-fluid" src={user.photoURL} alt="NTU Badge" />
                     </NavDropdown.Item>
@@ -46,7 +46,7 @@ const Header = () => {
                 </>
               }
               {
-                user.email ? <button className="btn btn-info ms-2 text-primary fw-bold badge rounded-pill btn-style" onClick={logOut}>SingOut</button> :
+                user.email ? <button style={{ color: 'white' }}  className="btn btn-info ms-2 fw-bold badge rounded-pill btn-style" onClick={logOut}>SingOut</button> :
                   <Link to="/Login">
                     <button className="btn btn-info text-light fw-bold badge rounded-pill btn-style">Login</button>
                   </Link>
